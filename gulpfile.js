58e28e298e6fd9085713bc326602de06edd7cf2d
Andreza Santana <andreza.santana16@gmail.com>
2020-08-03 13:21:21 -0300

    Adicionar Fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
 //compilar css
 function style() {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src([
+        'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
+        'src/scss/**/*.scss'])
         .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
         .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 }
@@ -24,6 +28,13 @@ function html() {
         .pipe(browserSync.stream());
 }
 
+//Copiar webfonts fontawesome
+function webfonts() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*.*')
+        .pipe(gulp.dest('dist/webfonts'))
+        .pipe(browserSync.stream());
+}
+
 //verificar alterações no scss
 function watch() {
     browserSync.init({
@@ -36,4 +47,4 @@ function watch() {
     gulp.watch('src/*.html', html);
 }
 
-exports.watch = gulp.series(style, script, html, watch);
+exports.watch = gulp.series(style, script, html, webfonts, watch);
