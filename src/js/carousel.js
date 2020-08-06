7ec3111c80a25c736fddc3dd53026d1661a63321
Andreza Santana <andreza.santana16@gmail.com>
2020-08-05 22:53:32 -0300

    Ajuste carrossel highlights

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,11 +1,11 @@
 $(function () {
-  $(".slide").slick({
+  $("#slide-1").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     centerMode: true,
-    prevArrow: $(".carousel-control-prev"),
-    nextArrow: $(".carousel-control-next"),
+    prevArrow: $("#control-prev-1"),
+    nextArrow: $("#control-next-1"),
     responsive: [
       {
         breakpoint: 640,
